da7b97aa622727358e97c001f8c3c3ff98c61b31
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-11 19:09:19 +0530

    WIP got dist folder to build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,7 +8,17 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }]
+    rules: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      loader: 'url-loader?limit=100000'
+    }]
   }
 };
 
