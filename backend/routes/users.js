5086fe421a503dba97ea54486f620dd63ba4280d
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-13 15:13:34 +0530

    add /login and /register endpoints

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,66 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable no-unused-vars */
+const express = require("express"),
+    router = express.Router(),
+    bcrypt = require("bcryptjs"),
+    jwt = require("jsonwebtoken"),
+    keys = require("../config/keys"),
+    // Load input validation
+    validateRegisterInput = require("../validation/register"),
+    validateLoginInput = require("../validation/login"),
+    // Load User model
+    { Customer, Vendor } = require("../models/User");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const ERROR_CODE = 400,
+    SALT_ROUNDS = 10;
+
+router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        res.status(ERROR_CODE).json(errors);
+        return;
+    }
+
+    const isVendor = req.body.type === "vendor",
+        model = isVendor ? Vendor : Customer;
+
+    model.findOne({ email: req.body.email }).then((user) => {
+        if (user) {
+            res.status(ERROR_CODE).json({ email: "Email already exists" });
+            return;
+        }
+
+        const obj = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        };
+
+        if (!isVendor) { obj.orders = []; }
+
+        // eslint-disable-next-line new-cap
+        const newUser = new model(obj);
+
+        bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (error, hash) => {
+                if (error) { throw error; }
+
+                newUser.password = hash;
+
+                newUser.save().then(usr => res.json(usr)).catch(errorObj => console.log(errorObj));
+            });
+        });
+    });
 });
 
-module.exports = router;
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+
+    if (!isValid) {
+        res.status(ERROR_CODE).json(errors);
+        return;
+    }
+
+    console.log("TOOD");
+});
