34a8aa516c39f618463512903e5d8f64dad3d1aa
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-13 10:45:07 +0530

    Add dispatch and cancel buttons

--- frontend/src/ViewProducts.js
+++ frontend/src/ViewProducts.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 
 class ProductListing extends Component {
     render() {
+        const { remaining } = this.props; let updateElm;
+
+        if (remaining) {
+            updateElm = (<React.Fragment> <span className="quantity-remaining"></span>
+                <button><i className="fas fa-bin"></i></button></React.Fragment>);
+        } else {
+            updateElm = <button><i className="fas fa-dispatch"></i></button>;
+        }
+
         return (
             <div>
                 <span className="name">{this.props.name}</span>
                 <span className="status"></span>
                 <span className="quantity"></span>
-                <span className="quantity-remaining"></span>
+                {updateElm}
             </div>
         );
     }
@@ -15,6 +24,7 @@ class ProductListing extends Component {
 
 ProductListing.propTypes = {
     name: String,
+    remaining: Number,
 };
 
 class ProductList extends Component {
