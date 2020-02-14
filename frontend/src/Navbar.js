e0ef68738a0dbb159f558b3681b7a4b0892f281e
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-14 14:29:35 +0530

    router and navbar merged and works

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import CreateModal from "./Create";
+import Modal from "./components/auth/Login";
 
-class Navbar extends Component {
-    render() {
-        return (<div className="navbar navbar-expand-lg navbar-dark bg-dark">
+const routing = (<Router>
+    <div>
+        <div className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="index.html">Shopping webapp</a>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -13,13 +16,21 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-
-                        <a className="nav-link" href="index.html">Home</a>
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create">Create product</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About</Link>
                     </li>
                 </ul>
             </div>
-        </div>);
-    }
-}
+        </div>
+        <Route path="/" exact component={Modal}></Route>
+        <Route path="/create" exact component={CreateModal}></Route>
+    </div>
+</Router>);
 
-export default Navbar;
+
+export default routing;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import CreateModal from "./Create";
-import Navbar from "./Navbar";
+
+import routing from "./Navbar";
 
 // routing setup from here
 // https://codeburst.io/getting-started-with-react-router-5c978f70df91
-
-const routing = (
-    <Router>
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/create">Create product</Link>
-                </li>
-                <li>
-                    <Link to="/about">About Us</Link>
-                </li>
-            </ul>
-            <Route path="/" component={Navbar}></Route>
-            <Route path="/create" exact component={CreateModal}></Route>
-        </div>
-    </Router>
-);
-
 
 ReactDOM.render(routing, document.getElementById("root"));
 
